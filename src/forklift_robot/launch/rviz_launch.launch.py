faf249e7ebbbef44c98d5e42a3ec738467527cf6
cangozpi <cangozpinar@gmail.com>
2022-11-28 14:30:43 +0300

    template xacro file created. launch file created for running robot_state_publisher node, rviz node, and joint_state_publisher_gui node. setup.py modified to include launch, urdf, and rviz directories inside the forklift_robot pkg.

---
+++ src/forklift_robot/launch/rviz_launch.launch.py
@@ -0,0 +1,75 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'forklift_robot'
+    urdf_file_name = 'forklift.urdf.xacro'
+
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), urdf_file_name)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+
+    # robot_state_publisher node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw,
+        'use_sim_time': True}] # add other parameters here if required
+    )
+
+
+    # rviz start node
+    rviz_config_file_name = 'forklift_rviz.rviz'
+    forklift_rviz_file_path = os.path.join(get_package_share_directory(pkg_name), rviz_config_file_name) #TODO: set this path to your .rviz configuration file path
+    rviz_start_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz2',
+        arguments=['-d', forklift_rviz_file_path] 
+    )
+
+
+
+    # joint_state_publisher_gui start node
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+    )
+    
+
+    # gazebo = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(
+    #         get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+    #     )
+
+
+    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    #                 arguments=['-topic', 'robot_description',
+    #                             '-entity', 'my_bot'],
+    #                 output='screen')
+
+
+
+
+    # Run the node
+    return LaunchDescription([
+        node_robot_state_publisher,
+        rviz_start_node,
+        joint_state_publisher_gui_node,
+        # gazebo,
+        # spawn_entity
+    ])
---
+++ src/forklift_robot/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+# Imports below are added by me
+import os
+from glob import glob
+from setuptools import find_packages
+
+package_name = 'forklift_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # ones below are added by me:
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), # includes files inside /launch directory
+        (os.path.join('share', package_name), glob('urdf/*')), # includes files inside /urdf directory
+        (os.path.join('share', package_name), glob('rviz/*')), # includes files inside /rviz directory
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cangozpi',
+    maintainer_email='cangozpinar@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
