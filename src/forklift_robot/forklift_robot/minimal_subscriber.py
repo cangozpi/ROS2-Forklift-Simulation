aecc15feb0017674b0d52a16eb1ef74d2fadfd95
cangozpi <cangozpinar@gmail.com>
2022-12-26 19:41:05 +0300

    subscriber to camera/image_raw successfully implemented. Camera feed can be successfully visualized using opencv.imshow().

--- src/forklift_robot/forklift_robot/minimal_subscriber.py
+++ src/forklift_robot/forklift_robot/minimal_subscriber.py
@@ -2,21 +2,33 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
-
+# from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('depth_camera_raw_image_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            'topic',
+            Image,
+            '/camera/image_raw',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.get_logger().info('HEYOOOO')
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        # self.get_logger().info(f'I heard:  {np.asarray(msg.data).shape}, {msg.height}, {msg.width}, {np.asarray(msg.data).shape[0] /(int(msg.height) * int(msg.width))}')
+        # self.get_logger().info(f'I heard:  {np.asarray(msg.data).reshape((3, msg.height, msg.width)).shape}')
+        # plt.imshow(np.asarray(msg.data).reshape((msg.height, msg.width, 3)))
+        # plt.show()
+        cv2.imshow('Forklift camera_raw_image message', np.asarray(msg.data).reshape((msg.height, msg.width, 3)))
+        cv2.waitKey(3)
+        self.get_logger().info('MAUW')
+
 
 
 def main(args=None):
