effcaa04bbdccef3938bf530e1e325d9eb7f70b6
cangozpi <cangozpinar@gmail.com>
2023-03-03 23:00:13 +0300

    sb3 merge works now

--- src/forklift_gym_env/forklift_gym_env/rl/DDPG_HER/utils.py
+++ src/forklift_gym_env/forklift_gym_env/rl/DDPG_HER/utils.py
@@ -38,13 +38,13 @@ def flatten_and_concatenate_observation(obs, env):
             obs['forklift_position_observation']['chassis_bottom_link']['pose']['orientation'].w, 
             
             # Add Twist/linear
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].x, 
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].y, 
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].z,
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].x, 
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].y, 
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['linear'].z,
             # Add Twist/angular
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].x, 
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].y, 
-            # obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].z,
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].x, 
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].y, 
+            obs['forklift_position_observation']['chassis_bottom_link']['twist']['angular'].z,
             ])
         obs_flattened = torch.concat((obs_flattened.reshape(-1), tf_obs.reshape(-1)), dim=0)
 
